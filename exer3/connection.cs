3d51e6f4f2836595c7943850069434d356b76557
Uyên Thy <uynthyee.313@gmail.com>
2024-10-09 13:06:22 +0700

    Add file Connect to SQL

---
+++ exer3/connection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace exer3
+{
+    internal class connection
+    {
+        private static string connectionstring = @"Data Source=UYNTHYEE\MSSQLSERVER01;Initial Catalog=account;Integrated Security=True;Trust Server Certificate=True";
+        public SqlConnection getConnection()
+        {
+            return new SqlConnection(connectionstring);
+        }
+    }
+}
