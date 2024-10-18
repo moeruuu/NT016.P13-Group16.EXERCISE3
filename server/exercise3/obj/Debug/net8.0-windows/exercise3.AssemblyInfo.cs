3972959735c059b54666f9a1c9710f0caf3c1957
Uyên Thy <uynthyee.313@gmail.com>
2024-10-18 23:25:05 +0700

    fix conflict

--- server/exercise3/obj/Debug/net8.0-windows/exercise3.AssemblyInfo.cs
+++ server/exercise3/obj/Debug/net8.0-windows/exercise3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercise3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+793989fdc09925cfa441487b8f521f4dbb012766")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c4c252928b4f32a8f86469eea0b8e20547abf85")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercise3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercise3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
