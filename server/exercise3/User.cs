f157ea5437ab49a69d1fc29baabb76d0fe9e585f
Uyên Thy <uynthyee.313@gmail.com>
2024-11-09 19:10:58 +0700

    Add database

---
+++ server/exercise3/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace exercise3
+{
+    public class User
+    {
+        public ObjectId UserId { get; set; }
+
+        public string Fullname { get; set; }
+       
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    
+        public string Email { get; set; }
+
+        public DateTime Birthday { get; set; }
+        
+        public bool logging { get; set; }
+        
+    }
+}
--- server/exercise3/obj/Debug/net8.0-windows/exercise3.AssemblyInfo.cs
+++ server/exercise3/obj/Debug/net8.0-windows/exercise3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercise3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c4c252928b4f32a8f86469eea0b8e20547abf85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf8908c9b815cdead11dd0b7ba0d078e6d699a56")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercise3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercise3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
