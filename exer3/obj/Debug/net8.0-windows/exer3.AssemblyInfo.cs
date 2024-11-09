ce503dfb4c2da2e303b3667e90721b3f17a90945
giang2005 <23520407@gm.uit.edu.vn>
2024-11-10 02:36:28 +0700

    Create search form

--- exer3/obj/Debug/net8.0-windows/exer3.AssemblyInfo.cs
+++ exer3/obj/Debug/net8.0-windows/exer3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exer3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c17eb1ee99c73e745ad05cbcbec6ef2b49d42ccf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f157ea5437ab49a69d1fc29baabb76d0fe9e585f")]
 [assembly: System.Reflection.AssemblyProductAttribute("exer3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exer3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
