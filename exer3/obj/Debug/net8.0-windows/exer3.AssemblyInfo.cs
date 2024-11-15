e7d9567522705e423c6a4c936e7c4006d21d7134
khsnhelovorld <23520718@ms.uit.edu.vn>
2024-11-15 14:47:52 +0700

    remove BookService class out of client

--- exer3/obj/Debug/net8.0-windows/exer3.AssemblyInfo.cs
+++ exer3/obj/Debug/net8.0-windows/exer3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exer3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd50c308fadc21e5442d886506cc4fab2f70b3b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29d0794e47c86ae2d0f921fd807ecc3a3572cc32")]
 [assembly: System.Reflection.AssemblyProductAttribute("exer3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exer3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
