fe460a808b48e6f4f3ee81e5db1e312bb38afd11
Uyên Thy <uynthyee.313@gmail.com>
2024-11-16 02:11:12 +0700

    check git

---
+++ server/exercise3/BookService.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Google.Apis.Auth.OAuth2;
+using static System.Formats.Asn1.AsnWriter;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Util.Store;
+
+namespace exercise3
+{
+    public class BookService
+    {
+        private static readonly string key = Environment.GetEnvironmentVariable("GOOGLE_KEY");
+        private readonly string clientid = Environment.GetEnvironmentVariable("GOOGLE_OAUTH_CLIENT_ID");
+        private readonly string clientsecret = Environment.GetEnvironmentVariable("GOOGLE_OAUTH_CLIENT_SECRET");
+        private static readonly string baseurl = @"https://www.googleapis.com/books/v1";
+
+
+        private async Task<string> GetAccessToken()
+        {
+            try
+            {
+                /*if (string.IsNullOrEmpty(clientid) || string.IsNullOrEmpty(clientsecret))
+                {
+                    MessageBox.Show("Vui lòng cài đặt client id hoặc secret");
+                }*/
+                var clientsecrets = new ClientSecrets
+                {
+                    ClientId = clientid,
+                    ClientSecret = clientsecret,
+                };
+                var flow = new GoogleAuthorizationCodeFlow(new GoogleAuthorizationCodeFlow.Initializer
+                {
+                    ClientSecrets = clientsecrets,
+                    Scopes = new[] { "https://www.googleapis.com/auth/books" }, // Sửa lại phạm vi này
+                    DataStore = new FileDataStore("Google.Apis.Auth")
+                });
+
+                var credential = await new AuthorizationCodeInstalledApp(flow, new LocalServerCodeReceiver()).AuthorizeAsync("user", CancellationToken.None);
+                return credential.Token.AccessToken;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+        public class Book
+        {
+            public string ID { get; set; }
+            public string Etag { get; set; }
+            public string Title { get; set; }
+            public List<string> Authors { get; set; }
+            public string Publisher { get; set; }
+            public string PublishedDate { get; set; }
+        }
+
+        public class BooksResponse
+        {
+            public BookItem[] items { get; set; }
+        }
+
+        public class BookItem
+        {
+            public VolumeInfo volumeInfo { get; set; }
+        }
+
+        public class VolumeInfo
+        {
+            public string id { get; set; }
+            public string etag { get; set; }
+            public string title { get; set; }
+            public List<string> authors { get; set; }
+            public string publisher { get; set; }
+            public string publishedDate { get; set; }
+        }
+
+       /* public string GetAuthorize()
+        {
+            *//*return $"https://accounts.google.com/o/oauth2/v2/auth?" +
+               $"scope=email&" +
+               $"response_type=code&" +
+               $"redirect_uri={uri}&" +
+               $"client_id={clientid}";*//*
+
+            //string redirectUri = "urn:ietf:wg:oauth:2.0:oob";
+            string redirectUri = "http://localhost:8080/";
+            *//*string oauth = string.Format("https://accounts.google.com/o/oauth2/auth?client_id={0}&redirect_uri={1}&scope={2}&response_type=code", clientid, redirectUri, scopes);
+            return oauth;*//*
+
+            return $"https://accounts.google.com/o/oauth2/v2/auth?" +
+           $"client_id={clientid}&" +
+           $"redirect_uri={Uri.EscapeDataString(redirectUri)}&" +
+           $"response_type=code&" +
+           $"scope={Uri.EscapeDataString(scopes)}";
+        }*/
+     
+        public async Task<List<Book>> SearchBooks(string search)
+        {
+            string apiUrl = $"https://www.googleapis.com/books/v1/volumes?q={search}&key={key}";
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    string accesstoken = await GetAccessToken();
+                    client.DefaultRequestHeaders.Add("Authorization", "Bearer " + accesstoken);
+                    var response = await client.GetStringAsync(apiUrl);
+                    var booksResponse = JsonConvert.DeserializeObject<BooksResponse>(response);
+
+                    List<Book> books = new List<Book>();
+                    
+
+                    foreach (var item in booksResponse.items)
+                    {
+                        books.Add(new Book
+                        {
+                            ID = item.volumeInfo.id,
+                            Etag = item.volumeInfo.etag,
+                            Title = item.volumeInfo.title,
+                            Authors = item.volumeInfo.authors,
+                            Publisher = item.volumeInfo.publisher,
+                            PublishedDate = item.volumeInfo.publishedDate
+                        });
+                    }
+
+                    return books;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+
+        }
+    }
+}
--- server/exercise3/obj/Debug/net8.0-windows/exercise3.AssemblyInfo.cs
+++ server/exercise3/obj/Debug/net8.0-windows/exercise3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercise3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c41cd8a464dccec08fe97a0e6453425300228c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0daf60ca3c37280806900692a0e0e4e823ce6df1")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercise3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercise3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
