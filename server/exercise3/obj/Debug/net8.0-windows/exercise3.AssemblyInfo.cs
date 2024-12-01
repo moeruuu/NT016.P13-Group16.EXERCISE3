da723461a7aa5859a4147027d492feb7a73b5dda
giang2005 <23520407@gm.uit.edu.vn>
2024-12-02 02:53:55 +0700

    Update change password

--- server/exercise3/obj/Debug/net8.0-windows/exercise3.AssemblyInfo.cs
+++ server/exercise3/obj/Debug/net8.0-windows/exercise3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exercise3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7df0efa9ec0f02e0b1e936837144aaf81467f67e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be8436bc2e92a9111b59cb58568ab91127e3afb")]
 [assembly: System.Reflection.AssemblyProductAttribute("exercise3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exercise3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
