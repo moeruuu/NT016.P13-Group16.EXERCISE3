97cb0269a7f6b1dcfa161dad2417ac480b6609d2
khsnhelovorld <23520718@ms.uit.edu.vn>
2024-12-03 21:45:49 +0700

    push

--- exer3/obj/Debug/net8.0-windows/exer3.AssemblyInfo.cs
+++ exer3/obj/Debug/net8.0-windows/exer3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exer3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80460b9b462001087577cea18a5d5972bf57a95c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d641f644129d37e9fa97e87357d5df6473fd05a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("exer3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exer3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
